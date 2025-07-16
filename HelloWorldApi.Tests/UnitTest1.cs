ba3e11a5538901dc7e3c225f020ed3780c356249
root <root@ip-172-31-22-190.eu-central-1.compute.internal>
2025-07-16 11:36:17 +0000

    AWS Transform commit for jobId cb483683-3487-455c-bb89-0fa10213b8ff

--- HelloWorldApi.Tests/UnitTest1.cs
+++ HelloWorldApi.Tests/UnitTest1.cs
@@ -1,5 +1,4 @@
 // File: MyFetcherTests.cs
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Moq.Protected;
 using System.Net;
@@ -7,6 +6,8 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using hello_world_api.Services;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HelloWorldApi.Tests
 {
@@ -14,14 +15,14 @@ namespace HelloWorldApi.Tests
     public class MyFetcherTests
     {
         [TestMethod]
-        public async Task FetchAsync_RealRequest()
+    public async Task FetchAsync_RealRequest()
         {
             var httpClient = new HttpClient();
             var fetcher = new MyFetcher(httpClient);
 
             var result = await fetcher.FetchAsync("https://webhook.site/02389481-94ae-4cf5-b9d4-6c3f6b9a1f7b/testt");
 
-            Assert.IsTrue(result != null); // You can tailor this check based on your webhook site response
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result != null); // You can tailor this check based on your webhook site response
         }
     }
 }
